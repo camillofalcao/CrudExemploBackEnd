0a423a905fb08e850fd9c24bd261dec8b148ec0e
Camillo <camillofalcao@gmail.com>
2023-10-31 11:14:16 -0300

    Primeiro commit

---
+++ Controllers/AtletaController.cs
@@ -0,0 +1,87 @@
+using AtletaApi.Models;
+using AtletaBackend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtletaBackend.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class AtletaController : ControllerBase
+{
+    public AtletaController(ApplicationDbContext db) => 
+        this.db = db;
+        
+    // GET: api/Atleta
+    [HttpGet]
+    public ActionResult<IEnumerable<Atleta>> Get()
+    {
+        if (db.Atletas == null)
+            return NotFound();
+
+        return db.Atletas;
+    }
+
+    // GET: api/Atleta/5
+    [HttpGet("{id}")]
+    public ActionResult<Atleta> GetId(string id)
+    {
+        var obj = db.Atletas.FirstOrDefault(x => x.Id == id);
+
+        if (obj == null)
+            return NotFound();
+
+        return obj;
+    }
+
+    // POST: api/Atleta
+    [HttpPost]
+    public ActionResult<Atleta> Post(Atleta obj)
+    {
+        if (string.IsNullOrWhiteSpace(obj.Id))
+            obj.Id = Guid.NewGuid().ToString();
+
+        db.Atletas.Add(obj);
+        db.SaveChanges();
+
+        return CreatedAtAction(
+            nameof(GetId),
+            new { id = obj.Id },
+            obj
+        );
+    }
+
+    // PUT: api/Atleta/5
+    [HttpPut("{id}")]
+    public IActionResult Put(string id, Atleta obj)
+    {
+        if (id != obj.Id)
+            return BadRequest();
+        
+        db.Atletas.Update(obj);
+        db.SaveChanges();
+
+        return NoContent();
+    }
+
+    // DELETE: api/Atleta/5
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {
+        if (db.Atletas == null)
+            return NotFound();
+
+        var obj = db.Atletas.FirstOrDefault(x => x.Id == id);
+
+        if (obj == null)
+            return NotFound();
+
+        db.Atletas.Remove(obj);
+        db.SaveChanges();
+
+        return NoContent();
+    }
+
+    private readonly ApplicationDbContext db;
+}
---
+++ Models/Atleta.cs
@@ -0,0 +1,8 @@
+namespace AtletaBackend.Models;
+public class Atleta
+{
+    public string? Id { get; set; }
+    public string Nome { get; set; } = "";
+    public double Altura { get; set; }
+    public double Peso { get; set; }
+}

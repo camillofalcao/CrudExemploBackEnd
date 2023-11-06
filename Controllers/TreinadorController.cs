e07b57e88f5aa9f1b0bbb97262ad6f450e4b1cbb
Camillo <camillofalcao@gmail.com>
2023-11-05 23:17:44 -0300

    Adicionado Controller para Treinador

---
+++ Controllers/TreinadorController.cs
@@ -0,0 +1,87 @@
+using AtletaApi.Models;
+using AtletaBackend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TreinadorBackend.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class TreinadorController : ControllerBase
+{
+    public TreinadorController(ApplicationDbContext db) => 
+        this.db = db;
+        
+    // GET: api/Treinador
+    [HttpGet]
+    public ActionResult<IEnumerable<Treinador>> Get()
+    {
+        if (db.Treinadores == null)
+            return NotFound();
+
+        return db.Treinadores;
+    }
+
+    // GET: api/Treinador/5
+    [HttpGet("{id}")]
+    public ActionResult<Treinador> GetId(string id)
+    {
+        var obj = db.Treinadores.FirstOrDefault(x => x.Id == id);
+
+        if (obj == null)
+            return NotFound();
+
+        return obj;
+    }
+
+    // POST: api/Treinador
+    [HttpPost]
+    public ActionResult<Treinador> Post(Treinador obj)
+    {
+        if (string.IsNullOrWhiteSpace(obj.Id))
+            obj.Id = Guid.NewGuid().ToString();
+
+        db.Treinadores.Add(obj);
+        db.SaveChanges();
+
+        return CreatedAtAction(
+            nameof(GetId),
+            new { id = obj.Id },
+            obj
+        );
+    }
+
+    // PUT: api/Treinador/5
+    [HttpPut("{id}")]
+    public IActionResult Put(string id, Treinador obj)
+    {
+        if (id != obj.Id)
+            return BadRequest();
+        
+        db.Treinadores.Update(obj);
+        db.SaveChanges();
+
+        return NoContent();
+    }
+
+    // DELETE: api/Treinador/5
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {
+        if (db.Treinadores == null)
+            return NotFound();
+
+        var obj = db.Treinadores.FirstOrDefault(x => x.Id == id);
+
+        if (obj == null)
+            return NotFound();
+
+        db.Treinadores.Remove(obj);
+        db.SaveChanges();
+
+        return NoContent();
+    }
+
+    private readonly ApplicationDbContext db;
+}
